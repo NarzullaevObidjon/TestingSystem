375c1d5e0f98b1792e7e516e2fc5c7d1e63f817f
Sarvarbek <maxmudovsarvarbek289@gmail.com>
2022-11-21 16:41:58 +0500

    userPdf added by Sarvarbek

--- src/main/java/com/company/service/UserService.java
+++ src/main/java/com/company/service/UserService.java
@@ -38,4 +38,6 @@ public interface UserService {
     boolean doesHave(String phone);
 
     boolean fillBalance(double money, int id);
+
+    boolean userPdf();
 }
--- src/main/java/com/company/service/UserServiceImpl.java
+++ src/main/java/com/company/service/UserServiceImpl.java
@@ -5,8 +5,16 @@ import com.company.dto.UserDTO;
 import com.company.entity.User;
 import com.company.util.Result;
 import com.google.gson.Gson;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.element.Table;
+import com.itextpdf.layout.properties.TextAlignment;
 
+import java.awt.*;
 import java.io.*;
+import java.util.List;
 
 public class UserServiceImpl implements UserService {
     @Override
@@ -42,6 +50,53 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    public boolean userPdf() {
+        Gson gson=new Gson();
+        File file=new File(Database.BASE_FOLDER,"users.pdf");
+
+        try (PdfWriter pdfWriter = new PdfWriter(file);
+             PdfDocument pdfDocument=new PdfDocument(pdfWriter);
+             Document document=new Document(pdfDocument)) {
+
+            Paragraph paragraph=new Paragraph("Users");
+            paragraph.setTextAlignment(TextAlignment.LEFT);
+            document.add(paragraph);
+
+
+
+            List<User> users = Database.users;
+
+            float []columns={20,50,50,50,50,50};
+
+            Table table=new Table(columns,true);
+
+            table.addCell("id");
+            table.addCell("name");
+            table.addCell("phone");
+            table.addCell("password");
+            table.addCell("balance");
+            table.addCell("type");
+
+            for (User user : users) {
+                table.addCell(String.valueOf(user.getId()));
+                table.addCell(user.getName());
+                table.addCell(user.getPhone());
+                table.addCell(user.getPassword());
+                table.addCell(String.valueOf(user.getBalance()));
+                table.addCell(String.valueOf(user.getType()));
+            }
+
+            document.add(table);
+            table.complete();
+
+        } catch (IOException e) {
+            return false;
+        }
+        return true;
+    }
+
+
+    @Override
     public User addUser(UserDTO userDTO) {
         User user = new User(userDTO.getName(), userDTO.getPhone(), userDTO.getPassword());
         Database.users.add(user);
@@ -58,4 +113,7 @@ public class UserServiceImpl implements UserService {
             throw new RuntimeException(e);
         }
     }
+
+
+
 }
